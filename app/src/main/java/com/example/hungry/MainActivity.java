901974b1d42de306ece30b2020d4faef719ffe4c
mblackbeak <bethralston@Beths-MacBook-Pro.local>
2022-11-25 15:23:31 -0800

    First commit

--- app/src/main/java/com/example/hungry/MainActivity.java
+++ app/src/main/java/com/example/hungry/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.hungry;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;//upvoted vs recent spinner
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set up spinner
+        spinner = findViewById(R.id.spinner);
+        String[] sValues = {"Most Recent", "Most Upvoted"};
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, sValues);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    //post your comment
+    public void post(View view){
+
     }
 }
