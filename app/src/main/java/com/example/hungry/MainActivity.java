c141b8ec05a5ba441bca5ff94884c3dec373986e
mblackbeak <bethmralston@gmail>
2022-11-29 13:18:07 -0800

    Updated likes

--- app/src/main/java/com/example/hungry/MainActivity.java
+++ app/src/main/java/com/example/hungry/MainActivity.java
@@ -10,8 +10,14 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -30,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> comments;//all the comments on a specific recipe
     ArrayList<Integer> likes;//the number of likes at index i corresponds to the comment at index i in comments
     ArrayList<LocalDateTime> postTimes;//the posted time for all comments
+    ArrayList<String> commentInfo;//comment info read from the txt file
     int first;//first comment index
     int second;//second comment index
     DateTimeFormatter formatter;//date time formatter
@@ -39,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO get intent
 
         //set up spinner
         spinner = findViewById(R.id.spinner);
@@ -58,7 +67,56 @@ public class MainActivity extends AppCompatActivity {
         comments = new ArrayList<String>();
         likes = new ArrayList<Integer>();
         postTimes = new ArrayList<LocalDateTime>();
+        commentInfo = new ArrayList<String>();
         formatter = DateTimeFormatter.ISO_DATE_TIME;
+
+        //Load comment data from output file and display 2 most recent if possible
+        String file = "output.txt";
+        String line = "";
+        //Read File and save data
+        try{
+            FileInputStream fis = openFileInput(file);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            while((line = br.readLine()) != null){
+                commentInfo.add(line);//add the comment information
+            }
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        //Split comment info into appropriate array lists
+        int infoIndex = 0;//index of comment info for while loop
+        while(infoIndex < commentInfo.size())
+        {
+            String info = commentInfo.get(infoIndex);//get the line
+            //add info to comments if line is a comment
+            if(info == "Comment")
+            {
+                comments.add(commentInfo.get(infoIndex + 1));
+            }else {
+                //add info to likes if line is like info
+                if (info == "Likes") {
+                    String tempLike = commentInfo.get(infoIndex + 1);
+                    likes.add(Integer.valueOf(tempLike));
+                }else {
+                    //add info to postTimes if line is datetime info
+                    if (info == "Time") {
+                        String tempTime = commentInfo.get(infoIndex + 1);
+                        LocalDateTime timeTemp = LocalDateTime.parse(tempTime, formatter);
+                        postTimes.add(timeTemp);
+                    }
+                }
+            }
+            infoIndex++;
+        }
+        //print out most recent comments
+        if(commentInfo.size() != 0)
+        {
+            displayRecent();
+        }
+
     }
 
     //post your comment
@@ -81,10 +139,12 @@ public class MainActivity extends AppCompatActivity {
     //filter button
     public void filter(View v){
         String spinVal = spinner.getSelectedItem().toString();
-        if(spinVal == "Most Recent")
-            displayRecent();
-        else
-            displayLiked();
+        if(comments.size() > 0) {
+            if (spinVal == "Most Recent")
+                displayRecent();
+            else
+                displayLiked();
+        }
     }
 
     //most recent
@@ -97,15 +157,21 @@ public class MainActivity extends AppCompatActivity {
         comment1.setText(com1);
         String lik1 = likes.get(first).toString();//number of likes
         like1.setText(lik1);
-        String post1 = postTimes.get(first).toString();//posted time
+        like1.setChecked(false);
+        LocalDateTime pTim = postTimes.get(first);
+        String post1 = pTim.format(formatter);////posted time
         pTime1.setText(post1);
         //second most recent comment
-        String com2 = comments.get(second);//2nd most recent comment
-        comment2.setText(com2);
-        String lik2 = likes.get(second).toString();//number of likes
-        like2.setText(lik2);
-        String post2 = postTimes.get(second).toString();//posted time
-        pTime2.setText(post2);
+        if(comments.size() > 1) {
+            String com2 = comments.get(second);//2nd most recent comment
+            comment2.setText(com2);
+            String lik2 = likes.get(second).toString();//number of likes
+            like2.setText(lik2);
+            like2.setChecked(false);
+            LocalDateTime pTime = postTimes.get(second);
+            String post2 = pTime.format(formatter);//get posted time//posted time
+            pTime2.setText(post2);
+        }
     }
 
     //most liked
@@ -124,21 +190,23 @@ public class MainActivity extends AppCompatActivity {
             }
             current++;
         }
-        String com1 = comments.get(first);//most recent comment
+        String com1 = comments.get(first);//most liked comment
         comment1.setText(com1);
         String lik1 = likes.get(first).toString();//number of likes
         like1.setText(lik1);
         like1.setChecked(false);
-        String post1 = postTimes.get(first).toString();//posted time
+        LocalDateTime pTime = postTimes.get(first);
+        String post1 = pTime.format(formatter);//get posted time//posted time
         pTime1.setText(post1);
-        //second most recent comment if more than one comment
+        //second most liked comment if more than one comment
         if(second != first) {
-            String com2 = comments.get(second);//2nd most recent comment
+            String com2 = comments.get(second);//2nd most liked comment
             comment2.setText(com2);
             String lik2 = likes.get(second).toString();//number of likes
             like2.setText(lik2);
             like2.setChecked(false);
-            String post2 = postTimes.get(second).toString();//posted time
+            LocalDateTime pTimeTwo = postTimes.get(second);
+            String post2 = pTimeTwo.format(formatter);//get posted time
             pTime2.setText(post2);
         }
         else
@@ -150,8 +218,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-    //increase likes
+    //increments likes for comment 1
     public void increaseLike1(View v) {
+        //if like 1 is checked add one like
+        //if like 1 is unchecked remove one like
         if (like1.isChecked()) {
             //get previous amount and add one
             Integer amount = likes.get(first) + 1;
@@ -165,7 +235,10 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+    //increments likes for comment 2
     public void increaseLike2(View v) {
+        //if like 2 is checked add one like
+        //if like 2 is unchecked remove one like
         if (like2.isChecked()) {
             //get previous amount and add one
             Integer amount = likes.get(second) + 1;
@@ -180,8 +253,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void save(){
+    //exit activity and save comment info to a txt file
+    public void save(View v){
+       // comment1.setText("YESSSS");
         String filename = "output.txt";
+        //get comment info
         int current = 0;
         String fileContent = "";
         while(current < comments.size()) {
@@ -189,11 +265,13 @@ public class MainActivity extends AppCompatActivity {
             String like = Integer.toString(likes.get(current));
             LocalDateTime currentTime = postTimes.get(current);
             String time = currentTime.format(formatter);
-            fileContent = fileContent + "Comment" + "\n" + comment + "\n" + "Likes" + "\n" + like + "\n" + "Time" + "\n" + time + "\n";
+            //add all info to a string that will save to the txt file
+            fileContent = fileContent + "Comment" + "\n" + comment + "\n" + "Likes" + "\n" + like + "\n" + "Time" + "\n" + time +"\n";
         }
         FileOutputStream outputStream;//Allow a file to be opened for writing
 
         try{
+            //save the file contents
             outputStream = openFileOutput(filename, Context.MODE_APPEND);
             outputStream.write(fileContent.getBytes());
             outputStream.close();
@@ -201,5 +279,6 @@ public class MainActivity extends AppCompatActivity {
         catch(Exception e){
             e.printStackTrace();
         }
+        //TODO!! finish intent
     }
 }
